97fe3f20c7f954599ce4051f5b622b5c551e6049
jinwook-song <wlsdnr129@naver.com>
2021-04-28 15:04:59 +0900

    lecture 3.1 nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,35 @@
+import { dbService } from "fbase";
+import { useState } from "react";
+
 // eslint-disable-next-line
-export default () => "Home";
+export default () => {
+  const [nweet, setNweet] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: new Date(),
+    });
+    setNweet("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
